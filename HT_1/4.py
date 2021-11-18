8789d64900e1adbdd6f21b4ed206e0e5ca0a287e
Volodymyr <karbivnychyi.volodymyr@gmail.com>
2021-11-18 14:12:30 +0200

    volodymyr_k - HT_1(update_#3)

--- HT_1/4.py
+++ HT_1/4.py
@@ -8,14 +8,12 @@ if number_of_string_from_user.isdigit():
     if int(number_of_string_from_user) > 0:
          
         # empty elements, for subsequent operations
-        list_of_strings_by_user = []
         concatenated_strings = ""
 
-        # get amount of strings equal of digit which we got from user and add they to the list
+        # get amount of strings equal of digit which we got from user and add concatenate they all into one string
         for i in range(int(number_of_string_from_user)):
             
             new_string_from_user = input("Please, input value to the string:")
-            list_of_strings_by_user.append(new_string_from_user)
             
             # joining the strings
             concatenated_strings = concatenated_strings + new_string_from_user
