ec2ffcf564fc93160eb219a3d934a25d5449d26b
Volodymyr <karbivnychyi.volodymyr@gmail.com>
2021-11-27 21:31:43 +0200

    volodymyr_k - ht_04(update_#2)

--- HT_04/4.py
+++ HT_04/4.py
@@ -3,3 +3,53 @@
 Написати функцію < prime_list >, яка прийматиме 2 аргументи - початок і кінець 
 діапазона, і вертатиме список простих чисел всередині цього діапазона.
 """
+
+# Data from user
+first_number = input("Please, input number (start range): ")
+second_number = input("Please, input number (end range): ")
+
+# Checking is input data are numbers only
+def isfloat(num_to_check):
+    try:
+        float(num_to_check)
+        return True
+    except ValueError:
+        return False
+
+# Searching all prime numbers inside specified range
+def prime_list(start_range, end_range):
+    
+    # Empty element, for subsequent operations
+    prime_numbers_list = []
+
+    # Checking is it numbers or no
+    if isfloat(start_range) == False or isfloat(end_range) == False:
+        prime_number_check = "Sorry, script accept digits only"
+    elif float(end_range) < 0:
+        prime_number_check = "Sorry, end of range must be bigger than 0"
+    else:
+        for item in range(int(start_range), int(end_range)):
+            # Cut simple option 
+            if item < 2:
+                prime_number_check = False
+
+            if item == 2 or item == 3:
+                prime_number_check = True
+                prime_numbers_list.append(item)
+
+            ### Real check of Prime number (Primality test)
+            elif item > 3:
+                counter = 0
+                for i in range(2, item):
+                    if item % i == 0:
+                        counter +=1
+                if counter == 0:
+                    prime_numbers_list.append(item)
+                else:
+                    prime_number_check = False
+
+        prime_number_check = prime_numbers_list
+    return prime_number_check
+
+# Function implementation
+print(prime_list(first_number, second_number))
--- HT_04/5.py
+++ HT_04/5.py
@@ -4,8 +4,57 @@
 і виводить всі числа Фібоначчі, що не перевищують його.
 """
 
-def fibonacci():
-    f = int(round(2*1.618, 0))
-    return f
+# Data from user
+user_number = input("Please, input one positive number: ")
 
-print(fibonacci())
+# Checking is input data are numbers only
+def isfloat(num_to_check):
+    try:
+        float(num_to_check)
+        return True
+    except ValueError:
+        return False
+
+# Calculate a new fibonacci numbers
+def fibonacci(inp_numb):
+
+    # Empty element, for subsequent operations
+    fibonacci_number_list = []
+    # First two elementa of fibonacci numbers
+    fibonacci_number_1 = 0
+    fibonacci_number_2 = 1
+
+    # Checking is it numbers or no
+    if isfloat(inp_numb) == False:
+        fibonacci_number_check = "Sorry, script accept digits only"
+    elif int(inp_numb) < 0:
+        fibonacci_number_check = "Sorry, script accept positive number only"
+    
+    elif int(inp_numb) == 0:
+        fibonacci_number_check = fibonacci_number_list[fibonacci_number_1]
+    elif int(inp_numb) == 1:
+        fibonacci_number_check = fibonacci_number_list[fibonacci_number_1, 
+                                                       fibonacci_number_2]
+    
+    ### Real check calculation
+    else:
+        counter = 0
+
+        # Calculate a new fibonacci numbers, based on two previous, 
+        # And replace our numbers on onw step (left)
+        while counter < int(inp_numb):
+
+            new_fibonacci_number = fibonacci_number_1 + fibonacci_number_2
+            fibonacci_number_list.append(new_fibonacci_number)
+
+            fibonacci_number_1 = fibonacci_number_2
+            fibonacci_number_2 = new_fibonacci_number
+
+            counter = fibonacci_number_2 + fibonacci_number_1
+
+        fibonacci_number_check = fibonacci_number_list
+
+    return fibonacci_number_check
+
+# Function implementation
+print(fibonacci(user_number))
