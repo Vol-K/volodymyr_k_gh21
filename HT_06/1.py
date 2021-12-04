acf47cd0f16e4628fd21576e484864f741b63d2c
Volodymyr <karbivnychyi.volodymyr@gmail.com>
2021-12-04 18:10:25 +0200

    voodymyr_k - ht_06(update_#1)

--- HT_06/1.py
+++ HT_06/1.py
@@ -8,29 +8,41 @@
 Через декілька ітерацій - відбувається зміна кольорів - 
 логіка така сама як і в звичайних світлофорах.
 Приблизний результат роботи наступний:
-      Red        Green
-      Red        Green
-      Red        Green
-      Red        Green
-      Yellow     Green
-      Yellow     Green
-      Green      Red
-      Green      Red
-      Green      Red
-      Green      Red
-      Yellow     Red
-      Yellow     Red
-      Red        Green
-      .......
+    Red        Green
+    Red        Green
+    Red        Green
+    Red        Green
+    Yellow     Green
+    Yellow     Green
+    Green      Red
+    Green      Red
+    Green      Red
+    Green      Red
+    Yellow     Red
+    Yellow     Red
+    Red        Green
+    .......
 """
 
 import time
 
-print("Print now")
-n = 1
-x = time.sleep(n)
-print(n)
-print("Printing after {} seconds".format(n))
+# Library with a traffic lights (in tuples) for 'Cars' & 'People'
+# First element from tuple its sygnal for 'Cars', second for 'People'
+traffic_light_lib = [("Red", "Green"), ("Red", "Green"), ("Red", "Green"),
+                     ("Red", "Green"), ("Yellow", "Green"), ("Yellow", "Green"),
+                     ("Green", "Red"), ("Green", "Red"), ("Green", "Red"),
+                     ("Green", "Red"), ("Yellow", "Red"), ("Yellow", "Red")]
 
-# логіка
-list of tuples
+# Showing traffic lites sygnal for 'cars' & 'peoples'
+def traffic_light():
+
+    # Timer of signal frequency (1 second)
+    signal_timer = 1
+    
+    for item in traffic_light_lib:
+        print('{:<8} {}'.format(item[0], item[1]))
+        time.sleep(signal_timer)
+
+# Function implementation
+while True:
+    traffic_light()
