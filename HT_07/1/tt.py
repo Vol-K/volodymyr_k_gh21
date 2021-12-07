2d5c816d7b572772b55a7fcb19a2adc27fa71f7b
Volodymyr <karbivnychyi.volodymyr@gmail.com>
2021-12-07 23:02:48 +0200

    volodymyr_k - ht_07(update_#1)

--- HT_07/1/tt.py
+++ HT_07/1/tt.py
@@ -7,11 +7,13 @@
     та історію транзакцій (файл <{username}_transactions.data>);
     - є можливість як вносити гроші, так і знімати їх. Обов'язкова перевірка введених даних 
     (введено число; знімається не більше, ніж є на рахунку).
+
 Особливості реалізації:
     - файл з балансом - оновлюється кожен раз при зміні балансу (містить просто цифру з балансом);
     - файл - транзакціями - кожна транзакція у вигляді JSON рядка додається в кінець файла;
     - файл з користувачами: тільки читається. Якщо захочете реалізувати функціонал додавання 
     нового користувача - не стримуйте себе :)
+
 Особливості функціонала:
     - за кожен функціонал відповідає окрема функція;
     - основна функція - <start()> - буде в собі містити весь workflow банкомата:
@@ -24,23 +26,85 @@
         2. Поповнити баланс
         3. Вихід
     - далі - фантазія і креатив :)
+    User-friendly буде, якщо ваш банкомат буде продовжувати роботу, 
+    доки користувач спеціально не обере меню "завершити роботу".
 """
 
-def start():
-    pass
+import os
+import json
+
 
 
 def transaction():
     pass
 
+# Checking username & password (is they valid) 
+def login(user_name, passwod):
+    
+    #target_path_1 = os.path.dirname(os.path.abspath(__file__))
 
-def login():
-    pass
+    users_database = "users.csv"
+    try:
+        with open(users_database, "r", encoding="utf-8") as file:
+            list_of_users = file.readlines()
+            
+            for lines in list_of_users:
+                lines = lines.split(",")
+                #if lines[0] == user_name:
+                #if lines[1] == passwod:
+                if lines[0] == user_name and lines[1] == passwod:
+                    f_result = True
+                else:
+                    f_result = False
+    except FileNotFoundError:
+        f_result = False
+
+    return f_result
+
+# 
+def view_balance(name):
+    print(name)
+    #file_name = f"{name}_balance.csv"
+    file_name = "thomas_balance.csv"
+    with open(file_name, "r", encoding="utf-8") as file:
+        xxx = file.readlines()
+        print(xxx)
+    
 
 
-def view_balance():
-    pass
+def update_balance(x):
+    #target_path_1 = os.path.join(os.path.dirname(__file__), x)
+    #target_path_1 = open(os.path.join('h222', x), 'r')
+    target_path_1 = os.path.dirname(os.path.abspath(__file__))
+    return target_path_1
 
 
-def update_balance():
-    pass
+def start():
+    # user_name = input("Input your name: ")
+    # user_pass = input("Input your password: ")
+    user_name = "Thomas"
+    user_pass = "ppassww"
+
+    if not login(user_name, user_pass):
+        print("Soory, wrong username or password, try again")
+    else:
+        print("You are logged in, please choose operation")
+        oparions = [[1, "Check balance"], [2, "Money operation"], [3, "Exit"]]
+        for item in oparions:
+            print(f"{item[0]} - {item[1]}")
+
+        user_option = input("Select 1, 2 or 3: ")
+        if int(user_option) == 1:
+            view_balance(user_name)
+        elif int(user_option) == 2:
+            update_balance(user_name)
+        elif int(user_option) == 3:
+            print("You came out, Good luck...")
+
+    return 
+
+start()
+
+#print(update_balance('users.csv'))
+
+#print(login('x', 'y'))
---
+++ HT_07/2/2.py
@@ -0,0 +1,123 @@
+""" Завдання_2
+
+Написати функцію, яка приймає два параметри: ім'я файлу та кількість символів.
+   На екран повинен вивестись список із трьома блоками - 
+   символи з початку, із середини та з кінця файлу.
+   Кількість символів в блоках - та, яка введена в другому параметрі.
+   Придумайте самі, як обробляти помилку, наприклад, коли кількість 
+   символів більша, ніж є в файлі (наприклад, файл із двох символів 
+   і треба вивести по одному символу, то що виводити на місці 
+   середнього блоку символів?)
+   В репозиторій додайте і ті файли, по яким робили тести.
+   Як визначати середину файлу (з якої брать необхідні символи) - 
+   кількість символів поділити навпіл, а отримане "вікно" символів 
+   відцентрувати щодо середини файла і взяти необхідну кількість. 
+   В разі необхідності заокруглення одного чи обох параметрів - 
+   дивіться на свій розсуд.
+
+   Наприклад:
+   █ █ █ ░ ░ ░ ░ ░ █ █ █ ░ ░ ░ ░ ░ █ █ █    - правильно
+                     ⏫ центр
+
+   █ █ █ ░ ░ ░ ░ ░ ░ █ █ █ ░ ░ ░ ░ █ █ █    - неправильно
+                     ⏫ центр
+"""
+
+# Output mistake that input number bigger than number of symbols in file
+class WrongNumber(Exception):
+    pass
+
+
+# # Data from user, file name & number of characters by this file
+# file_name = input("Please, input file name: ")
+# number_of_symbols = input("Please, input number of symbols: ")
+
+# Test input parameters to the function
+file_name = "symbols.txt"
+number_of_symbols = "3"
+
+# Find and get symbols from file (by 3 places).
+# From Start of file, Middle and End. 
+def get_symbols(*args):
+
+    # Checking input data, is available all parameters
+    if len(args) != 2:
+        func_result = "Something went wrong, please, input parameters correctly"
+    elif not args[1]:
+        func_result = "Wrong, please, input number correctly" 
+   
+    # If all is good, read the file and select our goals
+    else:
+        input_file_name = args[0]
+        input_numb_of_symbols = int(args[1])
+        if input_numb_of_symbols == 0:
+            func_result = "Wrong, please, input number bigger than zero"
+        else:
+            # Read the file and find our symbols for each blocks
+            try:
+                with open(input_file_name, "r", encoding="utf-8") as file:
+                    # Calculate real length of file
+                    file_symbols = len(file.read())
+                    file.seek(0)
+                    file_lines = file.readlines()
+                    file.seek(0)
+
+                    if len(file_lines) > 1:
+                        file_len = file_symbols + (len(file_lines) - 1)
+                    elif len(file_lines) == 1:
+                        file_len = file_symbols
+
+                    # Checking is we have enough symbols for output
+                    if input_numb_of_symbols > file_len:
+                        raise WrongNumber("Uncorrect input number")
+                    elif (input_numb_of_symbols * 3) > file_len:
+                        raise WrongNumber("Not enough symbols for output")
+
+                    else:
+                        # Start block
+                        start_file = file.read(input_numb_of_symbols)
+                        file.seek(0)
+
+                        # End block
+                        if len(file_lines) > 1:
+                            file.seek(file_len - input_numb_of_symbols)
+                        elif len(file_lines) == 1:
+                            file.seek(file_len - input_numb_of_symbols)                       
+                        end_file = file.read(-input_numb_of_symbols)
+                        file.seek(0)
+                        end_file = end_file.replace('\n', '\\n')
+
+                        # Middle block, 3 option by length of file & input number ()
+                        if (file_len % 2) == 0 and (input_numb_of_symbols % 2) == 0:
+                            position_of_mid_read = (file_len - input_numb_of_symbols) / 2
+                            file.seek(position_of_mid_read)
+                            mid_file = file.read(input_numb_of_symbols)
+                            file.seek(0)
+                            mid_file = mid_file.replace('\n', '\\n')
+
+                        elif (file_len % 2) > 0 and (input_numb_of_symbols % 2) > 0:
+                            position_of_mid_read = (file_len - (input_numb_of_symbols)) / 2
+                            file.seek(position_of_mid_read)
+                            mid_file = file.read(input_numb_of_symbols)
+                            file.seek(0)
+                            mid_file = mid_file.replace('\n', '\\n')
+                        
+                        # Expand middle range for correct output
+                        else:
+                            position_of_mid_read = (file_len - (input_numb_of_symbols + 1)) / 2
+                            file.seek(position_of_mid_read)
+                            mid_file = file.read(input_numb_of_symbols + 1)
+                            file.seek(0)
+                            mid_file = mid_file.replace('\n', '\\n')
+
+                func_result = f"Star of file:: '{start_file}', Middle:: '{mid_file}', End:: '{end_file}'".format(r'')
+
+            except FileNotFoundError:
+                func_result = "File doesn't exist, please input correct file name"
+          
+    return func_result
+     
+# Function implementation
+print(get_symbols(file_name, number_of_symbols))
+
+
